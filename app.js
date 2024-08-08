bf20c7e583bab62e2cc3f98222f2c44292d79108
Haluk Isik <isikhaluk@gmail.com>
2024-08-08 23:34:29 +0300

    PrismaLibSQL added to app.js

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ import { authenticateJWT, authenticateUser, generateToken } from './auth.js';
 // WebSocket
 import https from 'https';
 import { WebSocketServer } from 'ws';
+
+
+import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
+
 // import bodyParser from 'body-parser';
 
 // import AWS from 'aws-sdk';
@@ -48,7 +54,16 @@ const wss = new WebSocketServer({ server });
 dotenv.config();
 
 
-const prisma = new PrismaClient();
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+const prisma = new PrismaClient({ adapter })
+
+
+// const prisma = new PrismaClient();
 // const PORT = process.env.PORT;
 
 // Middleware to parse JSON bodies
