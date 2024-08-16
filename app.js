8883ca9a0e8517f5a2badce1f550fb7825dfdb71
Haluk Isik <isikhaluk@gmail.com>
2024-08-16 12:53:54 +0300

    websocket port changed to 3000

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/', routes);
 
 var server = http.createServer(app)
 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
